63208225b3b5f431c037605fb08affc9060ebc2b
Dmitrijs Voronovs <dmitrijs.voronovs@scandiweb.com>
2019-11-27 15:46:10 +0100

    new

---
+++ README.md
@@ -0,0 +1,8 @@
+# crumblepad
+Let's get ready to crumble!
+
+### installation
+1. `npm install -g ionic@latest`
+2. `git clone https://github.com/M-Frikken/crumblepad.git`
+3. `cd crumblepad`
+4. 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,28 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React from 'react';
+import FirstComponent from '../components/FirstComponent';
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Ionic Blank</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        Hey 
+        <p>
+          If you get lost, the{' '}
+          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
+            docs
+          </a>{' '}
+          will be your guide.
+        </p>
+        <FirstComponent />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
