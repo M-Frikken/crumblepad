f3c8ed70eeeea91299bac53567f521f1f31d3892
Dmitrijs Voronovs <dmitrijs.voronovs@scandiweb.com>
2019-12-15 20:46:43 +0100

    correct listeners

--- src/components/NoteListArchive.js
+++ src/components/NoteListArchive.js
@@ -3,14 +3,9 @@ import Note from './Note';
 import { IonList } from '@ionic/react';
 import '../styles/NoteList.css';
 import { useSelector } from 'react-redux';
-import { useFirebaseConnect } from 'react-redux-firebase';
 
 const NoteList = () => {
   const currentUserId = useSelector(state => state.firebase.auth.uid);
-  useFirebaseConnect([
-    { path: 'notes', queryParams: [ 'orderByChild=updatedAt' ] }
-    // { path: 'notes', queryParams: [ 'orderByChild=userId', `equalTo=${ currentUserId }`, 'orderByChild=updatedAt' ] }
-  ]);
   const notes = useSelector(state => state.firebase.data.notes) || {};
 
   const expiredNotes = Object.entries(notes).reduce((acc, [key, note]) => (
@@ -18,7 +13,6 @@ const NoteList = () => {
     ? { ...acc, [key]: note }
     : acc
   ), {});
-
 
   if (!Object.keys(expiredNotes).length) return <h4 className='empty' >Archive is empty...</h4>
 
