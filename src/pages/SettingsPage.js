6119b7ef15f7047f52f7fb186ab28933802ea03d
dmitrijs-voronovs <dmitrijs.voronovs@scandiweb.com>
2019-12-24 20:44:06 +0200

    Changed database structure and added support for the new structure

--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -1,13 +1,13 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { IonContent, IonItem, IonLabel, IonPage, IonReorder, IonReorderGroup } from '@ionic/react';
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { useFirebase } from 'react-redux-firebase';
 import { withRouter } from 'react-router';
 import { Redirect } from 'react-router-dom';
+import Loader from '../components/Loader';
 import { expirationOptions as defaultExpirationOptions } from '../components/Note';
 import PageHeader from '../components/PageHeader';
-import { useFirebase } from 'react-redux-firebase';
-import Loader from '../components/Loader';
 
 const SettingsPage = ({ location: { pathname }, match: { path } }) => {
   const firebase = useFirebase();
@@ -15,14 +15,14 @@ const SettingsPage = ({ location: { pathname }, match: { path } }) => {
   const settings = useSelector(({ firebase }) => firebase.data.settings) || {};
   const isLoading = useSelector(({ firebase }) => firebase.requesting['settings/3ndnPQPXbBRaGB17nRdQrUJkqHX2/expirationOptions']);
   
-  const { expirationOptions } = settings[uid] || {};
-  const opts = expirationOptions || defaultExpirationOptions;
-  const [expOptionOrder, setExpOptionOrder] = useState(Object.keys(opts));
+  const { expirationOptions: userExpirationOptions } = settings[uid] || {};
+  const expirationOptions = userExpirationOptions || defaultExpirationOptions;
+  const [expOptionOrder, setExpOptionOrder] = useState(Object.keys(expirationOptions));
 
   function doReorder(event) {
     const from = event.detail.from;
     // fix bug with more places then elements
-    const to = event.detail.to === Object.keys(opts).length ? event.detail.to - 1 : event.detail.to;
+    const to = event.detail.to === Object.keys(expirationOptions).length ? event.detail.to - 1 : event.detail.to;
     if (from === to) return event.detail.complete();
     
     const newOrder = expOptionOrder;
@@ -33,31 +33,44 @@ const SettingsPage = ({ location: { pathname }, match: { path } }) => {
     event.detail.complete();
   }
 
-  const [reorderLocked, reorderLockToggle] = useState(false);
-  const onToggleEdit = () => reorderLockToggle(!reorderLocked);
+  const [isOnSettingsPage, setIsOnSettingsPage] = useState(true);
 
   useEffect(() => {
+    // in order to render correctly expiration options after settings got chaged
+    if (pathname === path) setIsOnSettingsPage(true);
+    else setIsOnSettingsPage(false);
+
     return () => {
       if (pathname === path) {
         const convertedOptions = expOptionOrder.reduce((acc, pos, i) => (
-          { ...acc, [i]: opts[pos] }
+          { ...acc, [i]: expirationOptions[pos] }
         ), {});
         firebase.set(`settings/${uid}/expirationOptions`, convertedOptions);
       }
     }
   }, [pathname, path]);
 
+  const renderExpirationOptionSettings = () => {
+    if (!isOnSettingsPage) return null;
+   
+    return (
+      <IonReorderGroup disabled={false} onIonItemReorder={doReorder}>
+        {renderExpirationOptions()}
+      </IonReorderGroup>
+    );
+  };
+
   const renderExpirationOptions = () => {
     if (isLoading) return <Loader />;
 
-    return Object.entries(opts).map(([id, { title }]) => (
+    return Object.entries(expirationOptions).map(([id, { title }]) => (
       <IonItem key={id}>
         <IonLabel>{`${id} | ${title}`}</IonLabel>
         {/* <IonReorder slot="end" /> */}
         <IonReorder slot="start" />
       </IonItem>
     ));
-};
+  };
 
   if (!uid) return <Redirect to='/login' />
 
@@ -65,10 +78,8 @@ const SettingsPage = ({ location: { pathname }, match: { path } }) => {
     <IonPage>
       <PageHeader title='Settings'/>
       <IonContent>
-        <button onClick={ onToggleEdit }>Toggle</button>
-        <IonReorderGroup disabled={ reorderLocked } onIonItemReorder={doReorder}>
-        { renderExpirationOptions() }
-        </IonReorderGroup>
+        {/* <IonButton onClick={ onToggleEdit }>Toggle</IonButton> */}
+        { renderExpirationOptionSettings() }
       </IonContent>
     </IonPage>
   );
