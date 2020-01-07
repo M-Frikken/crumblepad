65cf9815bc5e542cb94c446692998c61465571cd
Dmitrijs Voronovs <dmitrijs.voronovs@scandiweb.com>
2020-01-07 21:26:51 +0200

    archive page fix add button

--- src/pages/ArchivePage.js
+++ src/pages/ArchivePage.js
@@ -9,30 +9,11 @@ const ArchivePage = () => {
   const uid = localStorage.getItem('uid');
   if (!uid) return <Redirect to='/login' />
 
-  function renderButton() {
-    return (
-      <IonFabList side="top">
-        <Link to="/note/-1">
-          <IonFabButton size="small"><IonIcon icon={ lock } /></IonFabButton>
-        </Link>
-        <Link to="/note/-2">
-          <IonFabButton size="small"><IonIcon icon={ timer } /></IonFabButton>
-        </Link>
-      </IonFabList>
-    )
-  }
-
   return (
     <IonPage>
       <PageHeader title="Archive" />
       <IonContent>
         <NoteListArchive />
-        <IonFab vertical="bottom" horizontal="end" slot="fixed">
-          <IonFabButton color="orange">
-            <IonIcon icon={ add } />
-          </IonFabButton>
-          { renderButton() }
-        </IonFab>
       </IonContent>
     </IonPage>
   );
