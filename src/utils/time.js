b433678e1b107bafe443466c8562fd552538e2f8
Dmitrijs Voronovs <dmitrijs.voronovs@scandiweb.com>
2020-01-08 23:25:13 +0200

    add short and full time string

--- src/utils/time.js
+++ src/utils/time.js
@@ -3,11 +3,18 @@ export const MINUTE_IN_MS = 60 * SECOND_IN_MS;
 export const HOUR_IN_MS = 60 * MINUTE_IN_MS;
 export const DAY_IN_MS = 24 * HOUR_IN_MS;
 
-export const getTimeStringFromNumbers = (days, hours, minutes, seconds) => (
-    `${plur(days, 'day')}${plur(hours, 'hour')}${plur(minutes, 'minute')}${plur(seconds, 'second')}`
+export const getTimeStringFromNumbers = (days, hours, minutes, seconds) => {
+    if (days) return `${plur(days, 'day')}`;
+    if (hours) return `${plur(hours, 'hour')}`;
+    if (minutes) return `${plur(minutes, 'minute')}`;
+    if (seconds) return `${plur(seconds, 'second')}`;
+};
+
+export const getFullTimeStringFromNumbers = (days, hours, minutes, seconds) => (
+    `${plur(days, 'day')}${plur(hours, 'hour')}${plur(minutes, 'minute')}${plur(seconds, 'second')}`.trim()
 );
 
-export const timeLeft = (timeInMs) => {
+export const timeLeft = (timeInMs, full = false) => {
     const difference = getTimeDifference(timeInMs);
     if (isExpired(timeInMs)) return '';
 
@@ -15,7 +22,9 @@ export const timeLeft = (timeInMs) => {
     const hours = Math.floor(difference / HOUR_IN_MS) % 24;
     const minutes = Math.floor(difference / MINUTE_IN_MS) % 60;
     const seconds = Math.floor(difference / SECOND_IN_MS) % 60;
-    return getTimeStringFromNumbers(days, hours, minutes, seconds);
+    return full
+        ? getFullTimeStringFromNumbers(days, hours, minutes, seconds)
+        : getTimeStringFromNumbers(days, hours, minutes, seconds);
 }
 
 export const getMsValueFromNumber = (days, hours, minutes, seconds) => (
